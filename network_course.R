7776e25beb0ac0c691541bbe73bd654b7bccfb85
c.emer09@gmail.com <c.emer09@gmail.com>
2018-10-29 11:56:07 -0300

    update

--- network_course.R
+++ network_course.R
@@ -1,51 +1,52 @@
----
-title: "Redes Complexas: Análises Básicas"
-author: "Carine Emer"
-date: "11/10/2018"
-output: html_document
-editor_options: 
-  chunk_output_type: console
----
-#######################################################################
+#########################################################################
+#  title: "Redes Complexas: Análises Básicas"
+#author: "Carine Emer"
+#date: "11/10/2018"
+#output: html_document
+#editor_options: 
+#chunk_output_type: console
+########################################################################
 
-Curso de Introdução à Teoria de Redes Ecológicas
+#Curso de Introdução à Teoria de Redes Ecológicas
 
-Todas as espécies interagem com alguma outra espécie na natureza. Nessa prática os alunos coletarão dados sobre interações mutualísticas na natureza (ex. formiga-planta, dispersor de semente-planta, polinizador-planta) e/ou utilizarão dados próprios ou da literatura para analisar métricas básicas de redes complexas.
+#Todas as espécies interagem com alguma outra espécie na natureza. Nessa prática os alunos
+#coletarão dados sobre interações mutualísticas na natureza (ex. formiga-planta, dispersor
+#de semente-planta, polinizador-planta) e/ou utilizarão dados próprios ou da literatura para
+#analisar métricas básicas de redes complexas.
 
-No final do curso o aluno será capaz de: 
-1) Desenhar uma rede de interações com diferentes pacotes
-2) Testar se rede é a conectada, modular ou aninhada com uso de modelos nulos
-3) Identificar a centralidade dos nodos
-4) Explorar a estrutura da rede, robustez e seu significado biológico
+#No final do curso o aluno será capaz de:
+#1) Desenhar uma rede de interações com diferentes pacotes
+#2) Testar se rede é a conectada, modular ou aninhada com uso de modelos nulos
+#3) Identificar a centralidade dos nodos
+#4) Explorar a estrutura da rede, robustez e seu significado biológico
 
 ##########################################################################
-__________________________________________________________________________
-Warning 1: this script works for both binary and weighted networks.
+#__________________________________________________________________________
+#Warning 1: this script works for both binary and weighted networks.
 
-Warning 2: there is no single magic way to draw all kinds of network.
-There are several network drawing methods implemented in R packages
-and stand-alone software. Study their logic and algorithms,
-see some papers in which they were used, think it through,
-and only then decide which drawing method to use in your study (M. Mello).
+#Warning 2: there is no single magic way to draw all kinds of network.
+#There are several network drawing methods implemented in R packages
+#and stand-alone software. Study their logic and algorithms,
+#see some papers in which they were used, think it through,
+#and only then decide which drawing method to use in your study.(M. Mello)
 
-Warning 3: Null models are an endless discussion in Ecology. The choice of null models depends on your question, but is essential to test the significance of the data. Chose carefully, based on the biological meanings of the randomization process.
+#Warning 3: Null models are an endless discussion in Ecology. The choice of null models
+#depends on your question, but is essential to test the significance of the data.
+#Chose carefully, based on the biological meanings of the randomization process.
 
-Warning 4: always check for updates of packages and R versions, specially if you start to have weird errors.
+#Warning 4: always check for updates of packages and R versions, specially if you start to have weird errors.
 
 #Have fun!
 
 #########################################################################
 
 
-```{r setup, echo=T, include = F}
 # Set the working directory
 #setwd("paste your working directory path here")
-#setwd("~/Documents/Documents - Girassol/Network course/Curso Redes UESC 2018/práticas R/data")
+setwd("~/Documents/Documents - Girassol/Network course/Curso Redes UESC 2018/práticas R/data")
 #knitr::opts_knit$set(root.dir = "~/Documents/Documents - Girassol/Network course/Curso Redes UESC 2018/práticas R/data")
-```
 
 
-```{r load packages, include = F, echo = T}
 # load packages
 library(bipartite)
 library(vegan)
@@ -53,9 +54,9 @@ library(reshape2)
 library(igraph)
 library(networkD3)
 library(reshape2)
-```
+#########################################################################
 
-```{r input data, echo=T, include = F}
+
 ### input data
 
 ### you can use the dataset available or your own data. To do so, just replace "the name within brackets" below for your won.
@@ -76,28 +77,25 @@ poll
 dim(poll)
 str(poll)
 
-```
 
-#################################
-Drawing networks
-#################################
-
-```{r plot networks bipartite}
+#########################################################################
+#Drawing networks
+#########################################################################
 
 #### Observe topological differences between binary and weighted networks for the different systems
 
 #### Invaded pollination network from a sub-alpine vegetation, New Zealand (Emer et al. 2016. DOI: 10.1111/ddi.12458)
 poll_network<-plotweb(poll,
-              ## set overall parameters 
-              text.rot=c(90), labsize=1,arrow="down.center",
-              y.width.low=0.05,y.width.high=0.05, ybig=1.8, low.y=1.2, high.y=2,
-              high.spacing=.01, low.spacing=0.01,
-              # shorter labels
-              #high.lablength=3, low.lablength=0, 
-              ### check the method if you prefer
-                   method="cca", 
-              ### set colors
-                  col.low="green", col.high="yellow", col.interaction = "grey80")
+                      ## set overall parameters 
+                      text.rot=c(90), labsize=1,arrow="down.center",
+                      y.width.low=0.05,y.width.high=0.05, ybig=1.8, low.y=1.2, high.y=2,
+                      high.spacing=.01, low.spacing=0.01,
+                      # shorter labels
+                      #high.lablength=3, low.lablength=0, 
+                      ### check the method if you prefer
+                      method="cca", 
+                      ### set colors
+                      col.low="green", col.high="yellow", col.interaction = "grey80")
 
 #### Network of ant-myrmecophyte interactions from a dam-fragmented island in Balbina, Central Amazon (Emer et al. 2013. DOI: 10.1111/cobi.12045).
 
@@ -108,6 +106,7 @@ poll_network<-plotweb(poll,
 
 # check size of the network by multiplying the n of rows and columns
 dim(ants) # 11*15 = 165
+
 #check the position of the interaction of interest, create the vector
 my.col<-c(rep("white",33),rep("blue",1),rep("white",131))                          
 myrm_network<-plotweb(ants,text.rot=c(90),            labsize=1,y.width.low=0.05,y.width.high=0.05,arrow="down.center",ybig=1.8, low.y=1.2, high.y=2, high.spacing=.01, low.spacing=0.01,method="cca",
@@ -116,32 +115,30 @@ myrm_network<-plotweb(ants,text.rot=c(90),            labsize=1,y.width.low=0.05
 
 #### Avian seed-dispersal network from Ilha Anchieta, Atlantic Forest (Bello et al. 2017. DOI doi/10.1002/ecy.1818/).
 seed_network<-plotweb(seed,text.rot=c(90),labsize=1,y.width.low=0.05,y.width.high=0.05,
-        arrow="down.center",ybig=1.8, low.y=1.2, high.y=2, high.spacing=.01, low.spacing=0.01,
-        method="cca", ### check the method if you prefer
-        col.low="green", col.high="yellow", col.interaction = "grey80")
+                      arrow="down.center",ybig=1.8, low.y=1.2, high.y=2, high.spacing=.01, low.spacing=0.01,
+                      method="cca", ### check the method if you prefer
+                      col.low="green", col.high="yellow", col.interaction = "grey80")
 
 #export the image
 #png('seed_network.png')
-       
+
 #dev.off()
 # Tip: There are many other parameters that can be set to customize the drawing.
 # Explore them!
 
 ### for examples of tri-trophic interactions and how to add abundances in the axis see "script_visual_Gruber_Safariland.R" (https://rdrr.io/cran/bipartite/man/plotweb.html)
-```
 
-```{r plot networks igraph}
+#########################################################################
 ### some more complex but flexible ways of drawing networks
-# ??str.igraph
 ### code needs update to differentiate plants and animals
 m<- read.csv("ants_Amazon.csv", head=T)
 clos<-read.csv("closeness_ants.csv", head=T)
 
 # transforms the column to a list of character strings
-  clos$species <- as.character(clos$species) 
+clos$species <- as.character(clos$species) 
 # transforms the column to a numeric vector
-  clos$closeness <- as.character(clos$closeness) 
-  clos$guild<-as.character(clos$guild) 
+clos$closeness <- as.character(clos$closeness) 
+clos$guild<-as.character(clos$guild) 
 
 ## transform to edgelist
 m1<-melt(m) 
@@ -149,18 +146,18 @@ m1<-melt(m)
 m2 <- subset(m1, value>0) 
 
 # sets the graph framework
-  g=graph.data.frame(m2,directed=FALSE) 
+g=graph.data.frame(m2,directed=FALSE) 
 #Check the main properties of the igraph object
 V(g)
 E(g)
 # ordering clos so that the order of species list matches the order of species in graph g
-  clos <- clos[order(match(clos$species, V(g)$name)),] 
-  clos$closeness <- as.numeric(clos$closeness)
+clos <- clos[order(match(clos$species, V(g)$name)),] 
+clos$closeness <- as.numeric(clos$closeness)
 
 # to a given value of closeness you associate a colour
-  #color <- colorRampPalette(c('white','blue')) # colour palette
+#color <- colorRampPalette(c('white','blue')) # colour palette
 # the colour vector for each species
-  closeness.col <- heat.colors(25)[as.numeric(cut(-clos$closeness, breaks = 25))] 
+closeness.col <- heat.colors(25)[as.numeric(cut(-clos$closeness, breaks = 25))] 
 
 
 ## to create a vector for guild
@@ -170,28 +167,28 @@ V(g)$shape[V(g)$guild=="ant"]="circle"
 V(g)$shape[V(g)$guild=="plant"]="square"
 
 plot(g,
-      # Set the drawing mode
-      # This package contains several drawing methods; try them!
+     # Set the drawing mode
+     # This package contains several drawing methods; try them!
      layout=layout_nicely,
-       # Set title
+     # Set title
      main='Ant-myrmecophyte network',
-       # Set node sizes
+     # Set node sizes
      vertex.size=6,
      # Set node attributes
      vertex.shape=V(g)$shape,
      vertex.size = 12,
      vertex.color = closeness.col,
      # Set link colors
-           edge.color = "lightblue",
-      # Set link curvature from 0 to 1
-           edge.curved=0.3,
-      # Set nodes labels
+     edge.color = "lightblue",
+     # Set link curvature from 0 to 1
+     edge.curved=0.3,
+     # Set nodes labels
      vertex.label.dist=200,
      vertex.label.color='black',
      vertex.label.font=1,
      vertex.label=V(g)$name,
      vertex.label.cex=0.5
-           )
+)
 #export the image
 #png('antplant_network.png')
 
@@ -204,35 +201,32 @@ col <- c("steelblue", "orange")
 shape <- c("circle", "square")
 
 V(g)$shape <- shape[as.numeric(V(g)$type) + 1]
-V(g)$color <- c('red', 'blue', 'green', 'steelblue', 'steelblue', 'steelblue', 'steelblue')
+V(g)$color <- col
 E(g)$color <- 'gray'
 plot(g)
 
-  
-```
 
-```{r plot networks network3D}
+#```{r plot networks network3D}
 n3D<-simpleNetwork(m2)
 n3D
 #saveNetwork(n3D, file = "ants_network3D.html") ### save network as html
-```
 
-#################################
-Network Structure and null models 
-#################################
 
-```{r metrics network structure}
+#########################################################################
+#Network Structure and null models 
+#########################################################################
 
-### Observe the differences in some metrics for the analyses of netwoks. 
-#Run the metrics for other systems, compare the topology among them. 
+
+### Observe the differences in some metrics for the analyses of netwoks.
+#Run the metrics for other systems, compare the topology among them.
 #What does it mean?
 
 #### Explore possibilities of different null models in the different packages, for example:
-??oecosimu
-??vegan::commsim
-??nullmodel
+#??oecosimu
+#??vegan::commsim
+#??nullmodel
 
-############## Degree distribution##########################
+############## Degree distribution ##########################
 #??degreedistr ## check the help for some limitations of this method
 
 poll.dd <- degreedistr(poll)
@@ -254,21 +248,21 @@ nested_ants<-networklevel(ants, index = "weighted NODF")
 nested_ants
 
 # Set Null Model
-  obs_nested<-nested_ants
-  null1<-oecosimu(ants,nestednodf,"quasiswap_count",nsimul = 100, order=FALSE)
-  null1
-  densityplot(permustats(null1))
+obs_nested<-nested_ants
+null1<-oecosimu(ants,nestednodf,"quasiswap_count",nsimul = 100, order=FALSE)
+null1
+densityplot(permustats(null1))
 
 #### Using the classical nullmodels for bipartite
 ## method 3 = vaznull, explore the other possibilities and discuss when to chose one or another
-  null <-nullmodel(ants, N=100, method=3) 
-  null1 <-sapply (X=null, FUN=nested, method="NODF2") 
-  means_null1 <- apply (X=cbind(null1),MARGIN=2, FUN=mean, na.rm=T)  
-  sd.means_null1 <- apply(X=cbind(null1), MARGIN=2, FUN=sd, na.rm=T)
-  z_score<-(obs_nested-means_null1)/sd.means_null1
-  z_score
-  p_value<-sum(null1>= obs_nested)/1000 # valor de p
-  p_value
+null <-nullmodel(ants, N=100, method=3) 
+null1 <-sapply (X=null, FUN=nested, method="NODF2") 
+means_null1 <- apply (X=cbind(null1),MARGIN=2, FUN=mean, na.rm=T)  
+sd.means_null1 <- apply(X=cbind(null1), MARGIN=2, FUN=sd, na.rm=T)
+z_score<-(obs_nested-means_null1)/sd.means_null1
+z_score
+p_value<-sum(null1>= obs_nested)/1000 # valor de p
+p_value
 
 plot(density(null1), lwd=2, xlim=c(0, 40))
 abline(v=obs_nested, col="red", lwd=1)
@@ -277,27 +271,25 @@ abline(v=obs_nested, col="red", lwd=1)
 m_ants<-computeModules(ants)
 m_ants
 # Check the components of each module
-  printoutModuleInformation(m_ants)
-  plotModuleWeb(m_ants)
+printoutModuleInformation(m_ants)
+plotModuleWeb(m_ants)
 
 # Set Null Model
-  nulls <- nullmodel(ants, N=100, method=1) 
-  modules.nulls <- sapply(nulls, computeModules)
-  like.nulls <- sapply(modules.nulls, function(x) x@likelihood)
-  z <- (m_ants@likelihood - mean(like.nulls))/sd(like.nulls)
-  p <- 2*pnorm(-abs(z))
-  plot(density(like.nulls), lwd=2, xlim=c(0, .8))
-  abline(v=m_ants@likelihood, col="red", lwd=1)
- 
-  
+nulls <- nullmodel(ants, N=100, method=1) 
+modules.nulls <- sapply(nulls, computeModules)
+like.nulls <- sapply(modules.nulls, function(x) x@likelihood)
+z <- (m_ants@likelihood - mean(like.nulls))/sd(like.nulls)
+p <- 2*pnorm(-abs(z))
+plot(density(like.nulls), lwd=2, xlim=c(0, .8))
+abline(v=m_ants@likelihood, col="red", lwd=1)
 
 #save.image("pratica_redes.RData")
-```
 
-#################################
-Centrality
-#################################
-```{r centrality}
+
+#########################################################################
+#CENTRALITY
+#########################################################################
+
 #### identify the degree, closess and betweenness of your species
 
 ## To test different indexes, you just have to change the index parameter. Level defines whether the index is calculated for the rows (lower) or columns (higher).
@@ -315,7 +307,6 @@ text(cz[[1]], cz[[2]], names(cz[[1]]), pos=4, cex=0.7)
 
 
 
-
 #### save the data
 #write.csv(clos_ants, "closeness_ants_higher.csv")
 
@@ -323,14 +314,12 @@ text(cz[[1]], cz[[2]], names(cz[[1]]), pos=4, cex=0.7)
 
 ### Save your results, so you dont have to run everything again once you want to update your code.
 
-save.image("pratica_redes.RData")
+#save.image("pratica_redes.RData")
 #dev.off()
-```
 
-###################################
-Robustness
-###################################
-```{r robustness}
+#########################################################################
+#NETWORK ROBUSTNESS
+#########################################################################
 #This index is based on:
 #Burgos, E., H. Ceva, R.P.J. Perazzo, M. Devoto, D. Medan, M. Zimmermann, and A. Maria Delbue (2007) Why nestedness in mutualistic networks? Journal of Theoretical Biology 249, 307ñ313
 #Slope.bipartite È baseado em 
@@ -342,7 +331,7 @@ Robustness
 par(mfrow=c(1,2))
 #retirada aleatória
 ants.extlower.r <- second.extinct(ants, participant="lower", 
-                                        method="random", nrep=100, details=FALSE)
+                                  method="random", nrep=100, details=FALSE)
 robustness(ants.extlower.r)
 #png(filename="dispersao1extlower_random.png", width = 500, height = 500)
 slope.bipartite(ants.extlower.r, pch=19, cex=.5)
@@ -354,13 +343,9 @@ ants.extlower.d <- second.extinct(ants, participant="lower", method="degree", nr
 robustness(ants.extlower.d)
 #png(filename="dispersao1extlower_degree.png", width = 500, height = 500)
 slope.bipartite(ants.extlower.d, pch=19, cex=0.5)
-
-save.image("pratica_redes.RData")
 #dev.off()
-
-```
 
 
 #########################################################################
-Now you are ready to explore other parameters of the networks and play with your drawings. Networks are a world of possibilities. Be creative and you will succeed!! 
+#Now you are ready to explore other parameters of the networks and play with your drawings. Networks are a world of possibilities. Be creative and you will succeed!! 
 #########################################################################
