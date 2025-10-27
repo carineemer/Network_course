98ab52c74c350a7778a96172e2ebb0b325118fbb
c.emer09@gmail.com <c.emer09@gmail.com>
2025-10-27 18:55:05 -0300

    modified folder name

---
+++ Curso_PPGEco_UFSC_2025/dataset/demo1.js
@@ -0,0 +1,233 @@
+var data=[["Cecropia concolor","Allomerus.octoarticulatus",0],
+["Cecropia purpureascens","Allomerus.octoarticulatus",0],
+["Cordia nodosa","Allomerus.octoarticulatus",0],
+["Duroia saccifera","Allomerus.octoarticulatus",2],
+["Hirtella duckei","Allomerus.octoarticulatus",15],
+["Hirtella myrmecophila","Allomerus.octoarticulatus",44],
+["Hirtella physophora","Allomerus.octoarticulatus",13],
+["Maieta guianensis","Allomerus.octoarticulatus",0],
+["Maieta poeppigii","Allomerus.octoarticulatus",0],
+["Tachigali myrmecophyla","Allomerus.octoarticulatus",0],
+["Tachigali polyphylla","Allomerus.octoarticulatus",0],
+["Cecropia concolor","Allomerus.septemarticulatus",0],
+["Cecropia purpureascens","Allomerus.septemarticulatus",0],
+["Cordia nodosa","Allomerus.septemarticulatus",0],
+["Duroia saccifera","Allomerus.septemarticulatus",4],
+["Hirtella duckei","Allomerus.septemarticulatus",0],
+["Hirtella myrmecophila","Allomerus.septemarticulatus",6],
+["Hirtella physophora","Allomerus.septemarticulatus",0],
+["Maieta guianensis","Allomerus.septemarticulatus",0],
+["Maieta poeppigii","Allomerus.septemarticulatus",0],
+["Tachigali myrmecophyla","Allomerus.septemarticulatus",0],
+["Tachigali polyphylla","Allomerus.septemarticulatus",0],
+["Cecropia concolor","Azteca.sp1",2],
+["Cecropia purpureascens","Azteca.sp1",0],
+["Cordia nodosa","Azteca.sp1",0],
+["Duroia saccifera","Azteca.sp1",0],
+["Hirtella duckei","Azteca.sp1",0],
+["Hirtella myrmecophila","Azteca.sp1",0],
+["Hirtella physophora","Azteca.sp1",0],
+["Maieta guianensis","Azteca.sp1",0],
+["Maieta poeppigii","Azteca.sp1",0],
+["Tachigali myrmecophyla","Azteca.sp1",0],
+["Tachigali polyphylla","Azteca.sp1",0],
+["Cecropia concolor","Azteca.sp5",0],
+["Cecropia purpureascens","Azteca.sp5",0],
+["Cordia nodosa","Azteca.sp5",15],
+["Duroia saccifera","Azteca.sp5",0],
+["Hirtella duckei","Azteca.sp5",0],
+["Hirtella myrmecophila","Azteca.sp5",0],
+["Hirtella physophora","Azteca.sp5",0],
+["Maieta guianensis","Azteca.sp5",0],
+["Maieta poeppigii","Azteca.sp5",0],
+["Tachigali myrmecophyla","Azteca.sp5",0],
+["Tachigali polyphylla","Azteca.sp5",0],
+["Cecropia concolor","Camponotus.balzanii",0],
+["Cecropia purpureascens","Camponotus.balzanii",1],
+["Cordia nodosa","Camponotus.balzanii",0],
+["Duroia saccifera","Camponotus.balzanii",0],
+["Hirtella duckei","Camponotus.balzanii",0],
+["Hirtella myrmecophila","Camponotus.balzanii",0],
+["Hirtella physophora","Camponotus.balzanii",0],
+["Maieta guianensis","Camponotus.balzanii",0],
+["Maieta poeppigii","Camponotus.balzanii",0],
+["Tachigali myrmecophyla","Camponotus.balzanii",0],
+["Tachigali polyphylla","Camponotus.balzanii",0],
+["Cecropia concolor","Crematogaster.erecta",0],
+["Cecropia purpureascens","Crematogaster.erecta",0],
+["Cordia nodosa","Crematogaster.erecta",0],
+["Duroia saccifera","Crematogaster.erecta",0],
+["Hirtella duckei","Crematogaster.erecta",0],
+["Hirtella myrmecophila","Crematogaster.erecta",0],
+["Hirtella physophora","Crematogaster.erecta",0],
+["Maieta guianensis","Crematogaster.erecta",0],
+["Maieta poeppigii","Crematogaster.erecta",0],
+["Tachigali myrmecophyla","Crematogaster.erecta",1],
+["Tachigali polyphylla","Crematogaster.erecta",0],
+["Cecropia concolor","Crematogaster.flavosensitiva",0],
+["Cecropia purpureascens","Crematogaster.flavosensitiva",0],
+["Cordia nodosa","Crematogaster.flavosensitiva",0],
+["Duroia saccifera","Crematogaster.flavosensitiva",0],
+["Hirtella duckei","Crematogaster.flavosensitiva",0],
+["Hirtella myrmecophila","Crematogaster.flavosensitiva",0],
+["Hirtella physophora","Crematogaster.flavosensitiva",0],
+["Maieta guianensis","Crematogaster.flavosensitiva",1],
+["Maieta poeppigii","Crematogaster.flavosensitiva",0],
+["Tachigali myrmecophyla","Crematogaster.flavosensitiva",0],
+["Tachigali polyphylla","Crematogaster.flavosensitiva",0],
+["Cecropia concolor","Myrmelachysta.flavocotea",0],
+["Cecropia purpureascens","Myrmelachysta.flavocotea",0],
+["Cordia nodosa","Myrmelachysta.flavocotea",0],
+["Duroia saccifera","Myrmelachysta.flavocotea",0],
+["Hirtella duckei","Myrmelachysta.flavocotea",0],
+["Hirtella myrmecophila","Myrmelachysta.flavocotea",0],
+["Hirtella physophora","Myrmelachysta.flavocotea",0],
+["Maieta guianensis","Myrmelachysta.flavocotea",0],
+["Maieta poeppigii","Myrmelachysta.flavocotea",0],
+["Tachigali myrmecophyla","Myrmelachysta.flavocotea",1],
+["Tachigali polyphylla","Myrmelachysta.flavocotea",0],
+["Cecropia concolor","Pachycondyla.unidentata",0],
+["Cecropia purpureascens","Pachycondyla.unidentata",0],
+["Cordia nodosa","Pachycondyla.unidentata",1],
+["Duroia saccifera","Pachycondyla.unidentata",0],
+["Hirtella duckei","Pachycondyla.unidentata",0],
+["Hirtella myrmecophila","Pachycondyla.unidentata",0],
+["Hirtella physophora","Pachycondyla.unidentata",0],
+["Maieta guianensis","Pachycondyla.unidentata",0],
+["Maieta poeppigii","Pachycondyla.unidentata",0],
+["Tachigali myrmecophyla","Pachycondyla.unidentata",0],
+["Tachigali polyphylla","Pachycondyla.unidentata",0],
+["Cecropia concolor","Pheidole.minutula",0],
+["Cecropia purpureascens","Pheidole.minutula",0],
+["Cordia nodosa","Pheidole.minutula",0],
+["Duroia saccifera","Pheidole.minutula",0],
+["Hirtella duckei","Pheidole.minutula",0],
+["Hirtella myrmecophila","Pheidole.minutula",0],
+["Hirtella physophora","Pheidole.minutula",0],
+["Maieta guianensis","Pheidole.minutula",35],
+["Maieta poeppigii","Pheidole.minutula",21],
+["Tachigali myrmecophyla","Pheidole.minutula",0],
+["Tachigali polyphylla","Pheidole.minutula",0],
+["Cecropia concolor","Pseudomyrmex.concolor",0],
+["Cecropia purpureascens","Pseudomyrmex.concolor",0],
+["Cordia nodosa","Pseudomyrmex.concolor",0],
+["Duroia saccifera","Pseudomyrmex.concolor",0],
+["Hirtella duckei","Pseudomyrmex.concolor",0],
+["Hirtella myrmecophila","Pseudomyrmex.concolor",0],
+["Hirtella physophora","Pseudomyrmex.concolor",0],
+["Maieta guianensis","Pseudomyrmex.concolor",0],
+["Maieta poeppigii","Pseudomyrmex.concolor",0],
+["Tachigali myrmecophyla","Pseudomyrmex.concolor",13],
+["Tachigali polyphylla","Pseudomyrmex.concolor",0],
+["Cecropia concolor","Pseudomyrmex.nigrescens",0],
+["Cecropia purpureascens","Pseudomyrmex.nigrescens",0],
+["Cordia nodosa","Pseudomyrmex.nigrescens",0],
+["Duroia saccifera","Pseudomyrmex.nigrescens",0],
+["Hirtella duckei","Pseudomyrmex.nigrescens",0],
+["Hirtella myrmecophila","Pseudomyrmex.nigrescens",0],
+["Hirtella physophora","Pseudomyrmex.nigrescens",0],
+["Maieta guianensis","Pseudomyrmex.nigrescens",0],
+["Maieta poeppigii","Pseudomyrmex.nigrescens",0],
+["Tachigali myrmecophyla","Pseudomyrmex.nigrescens",5],
+["Tachigali polyphylla","Pseudomyrmex.nigrescens",0],
+["Cecropia concolor","Pseudomyrmex.sp1",0],
+["Cecropia purpureascens","Pseudomyrmex.sp1",0],
+["Cordia nodosa","Pseudomyrmex.sp1",0],
+["Duroia saccifera","Pseudomyrmex.sp1",0],
+["Hirtella duckei","Pseudomyrmex.sp1",0],
+["Hirtella myrmecophila","Pseudomyrmex.sp1",0],
+["Hirtella physophora","Pseudomyrmex.sp1",0],
+["Maieta guianensis","Pseudomyrmex.sp1",0],
+["Maieta poeppigii","Pseudomyrmex.sp1",0],
+["Tachigali myrmecophyla","Pseudomyrmex.sp1",0],
+["Tachigali polyphylla","Pseudomyrmex.sp1",1],
+["Cecropia concolor","Solenopsis.sp1",0],
+["Cecropia purpureascens","Solenopsis.sp1",0],
+["Cordia nodosa","Solenopsis.sp1",1],
+["Duroia saccifera","Solenopsis.sp1",0],
+["Hirtella duckei","Solenopsis.sp1",0],
+["Hirtella myrmecophila","Solenopsis.sp1",0],
+["Hirtella physophora","Solenopsis.sp1",0],
+["Maieta guianensis","Solenopsis.sp1",0],
+["Maieta poeppigii","Solenopsis.sp1",0],
+["Tachigali myrmecophyla","Solenopsis.sp1",0],
+["Tachigali polyphylla","Solenopsis.sp1",0],
+["Cecropia concolor","Solenopsis.sp4",0],
+["Cecropia purpureascens","Solenopsis.sp4",0],
+["Cordia nodosa","Solenopsis.sp4",1],
+["Duroia saccifera","Solenopsis.sp4",0],
+["Hirtella duckei","Solenopsis.sp4",0],
+["Hirtella myrmecophila","Solenopsis.sp4",0],
+["Hirtella physophora","Solenopsis.sp4",0],
+["Maieta guianensis","Solenopsis.sp4",0],
+["Maieta poeppigii","Solenopsis.sp4",0],
+["Tachigali myrmecophyla","Solenopsis.sp4",0],
+["Tachigali polyphylla","Solenopsis.sp4",0]]
+
+
+
+ function sort(sortOrder){
+                    return function(a,b){ return d3.ascending(sortOrder.indexOf(a),sortOrder.indexOf(b)) }
+                  }
+var color = {'Unlinked ':'#3366CC','Cecropia concolor':'#7FC97F','Cecropia purpureascens':'#BEAED4','Cordia nodosa':'#FDC086','Duroia saccifera':'#FFFF99','Hirtella duckei':'#386CB0','Hirtella myrmecophila':'#F0027F','Hirtella physophora':'#BF5B17','Maieta guianensis':'#666666','Maieta poeppigii':'#7FC97F','Tachigali myrmecophyla':'#BEAED4','Tachigali polyphylla':'#FDC086'};
+
+
+
+
+var g1 = svg.append("g").attr("transform","translate(303,50)");
+                         var bp1=viz.bP()
+                         .data(data)
+                         .value(d=>d[2])
+                         .min(10)
+                         .pad(1)
+                         .height(400)
+                         .width(200)
+                         .barSize(35)
+                         .fill(d=>color[d.primary])
+.orient("vertical");
+
+g1.call(bp1);g1.append("text")
+                        .attr("x",-50).attr("y",-8)
+                        .style("text-anchor","middle")
+                        .text("Plants");
+                        g1.append("text")
+                        .attr("x", 250)
+                        .attr("y",-8).style("text-anchor","middle")
+                        .text("Ants");
+                        g1.append("text")
+                        .attr("x",100).attr("y",-25)
+                        .style("text-anchor","middle")
+                        .attr("class","header")
+                        .text("Ant-myrmecophyte network 3D");
+
+ g1.selectAll(".mainBars")
+                        .on("mouseover",mouseover)
+                        .on("mouseout",mouseout);
+
+ g1.selectAll(".mainBars").append("text").attr("class","label")
+                        .attr("x",d=>(d.part=="primary"? -46.4:53.6))
+                        .attr("y",d=>+6)
+                        .text(d=>d.key)
+                        .attr("text-anchor",d=>(d.part=="primary"? "end": "start"));
+
+ g1.selectAll(".mainBars").append("text").attr("class","perc")
+                        .attr("x",d=>(d.part=="primary"? -237:288))
+                        .attr("y",d=>+6)
+                        .text(function(d){ return d3.format("0.0%")(d.percent)})
+                        .attr("text-anchor",d=>(d.part=="primary"? "end": "start")); 
+
+function mouseover(d){
+bp1.mouseover(d);
+                            g1.selectAll(".mainBars")
+                            .select(".perc")
+                            .text(function(d){ return d3.format("0.0%")(d.percent)});
+}
+
+                     function mouseout(d){
+bp1.mouseout(d);
+                            g1.selectAll(".mainBars")
+                            .select(".perc")
+                            .text(function(d){ return d3.format("0.0%")(d.percent)});
+}
+
+
